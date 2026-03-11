0be9d52fe10c0c59e6d4c652e39ded16c681cf0e
Rafael <rafaelsatrio44@gmail.com>
2026-03-11 18:07:28 +0800

    fix: correct module path for ensure_budget_control_settings in hooks

--- imogi_finance/hooks.py
+++ imogi_finance/hooks.py
@@ -425,7 +425,7 @@ before_migrate = [
 after_migrate = [
     "imogi_finance.utils.ensure_coretax_export_doctypes",
     "imogi_finance.utils.ensure_advances_allow_on_submit",
-    "imogi_finance.utils.ensure_budget_control_settings",
+    "imogi_finance.imogi_finance.utils.ensure_budget_control_settings",
 ]
 
 # Overriding Methods
