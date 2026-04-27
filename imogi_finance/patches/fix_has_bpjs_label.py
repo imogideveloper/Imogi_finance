deff4b339eb1457e9f24176e983411559c0e279c
Rafael <arap-boop@github.com>
2026-04-27 13:16:42 +0800

    fix: add patch to update has_bpjs label and position

---
+++ imogi_finance/patches/fix_has_bpjs_label.py
@@ -0,0 +1,9 @@
+import frappe
+
+def execute():
+    if frappe.db.exists("Custom Field", "Employee-has_bpjs"):
+        frappe.db.set_value("Custom Field", "Employee-has_bpjs", {
+            "label": "Bebas BPJS",
+            "insert_after": "exempt_from_pph21"
+        })
+        frappe.db.commit()
